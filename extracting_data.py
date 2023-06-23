9478aa9102636d9472bb654ac68d1a5b3eb70fb5
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-06-23 16:13:33 +0100

    added token calculation functionnality

--- extracting_data.py
+++ extracting_data.py
@@ -44,9 +44,26 @@ for i, doc in divided_documents:
         if header not in grouped_documents:
             grouped_documents[header] = []
         grouped_documents[header].append((i, content))
-
+"""
 # Print the grouped documents
 for header, docs in grouped_documents.items():
     print(f"Header: {header}\n")
     for i, doc_content in docs:
-        print(f"Document {i}:\n{doc_content.strip()}\n")
+        print(f"Document {i}:\n{doc_content.strip()}\n")"""
+
+##Calculer les tokens
+import tiktoken
+
+enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
+tokens_per_document = []
+
+for header, docs in grouped_documents.items():
+    for i, doc_content in enumerate(docs, 1):
+        doc_content_str = str(doc_content)  # Convert doc_content to string
+        tokens = enc.encode(doc_content_str)
+        tokens_per_document.append((header, i, len(tokens)))
+
+# Print the number of tokens in each document
+for header, doc_num, tokens in tokens_per_document:
+    print(f"Header: {header}")
+    print(f"Document {doc_num}: {tokens} tokens\n")
