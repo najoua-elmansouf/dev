302f0d4d274a92958bc95206dc14d0edb8df6a38
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-06-23 15:11:01 +0100

    extract the data from the pdf file then divide it

---
+++ extracting_data.py
@@ -0,0 +1,34 @@
+import pdfplumber
+
+##divide the content based on the headers
+def divide_content_by_header(content):
+    lines = content.split('\n')  # Split the content into lines
+
+    documents = []
+    current_document = []
+
+    for line in lines:
+        if line.strip():  # Skip empty lines
+            current_document.append(line)
+            if len(current_document) == 1:  # Check if it's the first line of a document
+                if current_document != [line]:  # Check if it's not the first document
+                    documents.append('\n'.join(current_document[:-1]))  # Add previous document
+                    current_document = [line]  # Start new document with the current line
+
+    if current_document:
+        documents.append('\n'.join(current_document))
+
+    return documents
+
+##extract pdf doc data
+with open('C:/Users/rita/Downloads/CoC_LegislativeGuide.pdf', 'rb') as file:    
+    # pdfplumber
+    pdf = pdfplumber.open(file)
+    text = ''
+    tables = []
+    for page in pdf.pages:
+        text += page.extract_text()
+        page_tables = page.extract_tables()
+        tables.extend(page_tables)
+    pdf.close()
+
--- model.py
+++
@@ -1,9 +0,0 @@
-import pdfplumber
-
-with open('C:/Users/rita/Downloads/CoC_LegislativeGuide.pdf', 'rb') as file:    
-    # pdfplumber
-    pdf = pdfplumber.open(file)
-    text = ''
-    for page in pdf.pages:
-        text += page.extract_text()
-    pdf.close()
