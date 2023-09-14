01b3cd3552361a26f1e36ab4c2dc35116319eb71
gh-ita <hatimi.ghita@ensam-casa.ma>
2023-09-14 11:10:39 +0100

    okey

--- internship/internship/urls.py
+++ internship/internship/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
 ]
 
 # Add the following lines to serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 """internship URL Configuration
